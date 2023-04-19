f2c204ccd4c8ea6f4f86a41d8e3ecb5632efc140
itsarijitdebnath@gmail.com <itsarijitdebnath@gmail.com>
2023-04-20 01:29:10 +0530

    push

---
+++ pages/orator2.js
@@ -0,0 +1,44 @@
+import { useState, useEffect } from 'react';
+
+import { useRouter } from 'next/router';
+
+const Redirect = () => {
+
+  const [isLoading, setIsLoading] = useState(true);
+
+  const router = useRouter();
+
+  useEffect(() => {
+
+    // Replace the URL below with the desired URL that you want to redirect to
+
+    router.push('https://discord.com/oauth2/authorize?client_id=1055929812910346241&permissions=2184535104&scope=applications.commands%20bot');
+
+  }, []);
+
+  return (
+
+    <div>
+
+      {isLoading && (
+
+        <div className="loading-spinner">
+
+          {/* Add your loading spinner component here */}
+
+          <div className="spinner"></div>
+
+          <p>Loading...</p>
+
+        </div>
+
+      )}
+
+    </div>
+
+  );
+
+};
+
+export default Redirect;
+
