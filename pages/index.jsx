03f5cf23ef5d3de75d769decd0f8ac3bfcd013da
itsarijitdebnath@gmail.com <itsarijitdebnath@gmail.com>
2023-04-20 01:20:20 +0530

    Vote Link Update

--- pages/index.jsx
+++ pages/index.jsx
@@ -27,7 +27,7 @@ export default function Index() {
               </a>
             </Link>
   <div className="py-10"></div>
-  <Link href={"https://oratorbot.xyz/vote"}>
+  <Link href={"https://top.gg/bot/948637316145102868/vote"}>
   <a className={"flex items-center px-6 justify-center gap-x-2 shadow-lg shadow-amber-600/20 rounded-xl py-4 font-medium bg-gradient-to-bl from-amber-700 to-amber-500 hover:opacity-80 transition duration-200 text-white " }>
 Vote Orator</a>
   </Link>
